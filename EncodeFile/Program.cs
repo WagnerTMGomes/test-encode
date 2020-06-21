33851f7b362f24ba1659fe4bd80aad28e9bd3bd2
wagner <wagner.thadeu@gmail.com>
2020-06-21 03:52:51 +0100

    Escrita de logs OK Encode OK

---
+++ EncodeFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using log4net;
+using log4net.Config;
+
+namespace EncodeFile
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            log.Info("Info message");
+
+            string filename;
+
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter valid path.");
+                filename = ConfigurationManager.AppSettings["filePath"];
+                //return;
+            }
+            else {
+                filename = args[0];
+            }
+            
+                       
+            Byte[] bytes = File.ReadAllBytes(filename);
+            String outputFile = Convert.ToBase64String(bytes);
+
+            System.Console.WriteLine(outputFile);
+
+
+        }
+    }
+}
